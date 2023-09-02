df8f6da00836588ef81cc7758bfe957e64ac9e3a
Kelvin Fernandes <fernandeskelvinkwf@gmail.com>
2023-09-02 11:09:23 -0300

    add comando e otimizando codigo

--- jogo_da_forca.py
+++ jogo_da_forca.py
@@ -2,26 +2,22 @@ import random
 import os
 from time import sleep
 
-def painel():
+def limpar_tela():
     os.system('cls' if os.name == 'nt' else 'clear')
-    print(f'Você só pode errar {tentativas} vezes até acertar a palavra!\n')
-    print(" ".join(lugar_para_encontrar))
-    letra_escolhida = input('\nEscolha uma letra: ').lower()
-    return letra_escolhida
 
-def painel_errado():
-    os.system('cls' if os.name == 'nt' else 'clear')
-    print(f'Você só pode errar {tentativas} vezes até acertar a palavra!\n')
+def mostrar_painel():
+    limpar_tela()
+    print(f'Você só pode errar {tentativas} vezes até acertar a palavra! \U0001f6a8\n')
     print(" ".join(lugar_para_encontrar))
-    print('\n\nLetras erradas: ' + ' - '.join(letra.upper() for letra in lista_errada))
-    letra_escolhida = input('\nEscolha uma letra: ').lower()
-    return letra_escolhida
-
-nome_usuario = input('Olá, qual é o seu nome? ')
-print(f'\nOlá, {nome_usuario}! Seja Bem-vinda(o) ao Jogo da Forca!\U0001F600 \n\n')
+    if len(lista_errada) != 0:
+        print('\n\n\u274C Letras erradas: ' + ' - '.join(letra.upper() for letra in lista_errada))
 
 palavras = ['banana', 'laranja', 'maça', 'uva', 'morango',
-'abacaxi', 'limao', 'pera', 'kiwi', 'melancia']
+'abacaxi', 'limao', 'pera', 'kiwi', 'melancia', 'manga', 'goiaba',
+'ameixa', 'cereja']
+
+nome_usuario = input('Olá, qual é o seu nome? ')
+print(f'\nOlá, {nome_usuario}! Seja Bem-vinda(o) ao Jogo da Forca! \U0001f609\n\n')
 
 while True:
     palavra_escolhida = random.choice(palavras)
@@ -32,17 +28,20 @@ while True:
     tentativas = 6
     contador_erros = 0
 
-    print(f'Sua palavra tem {numero_letras} letras!\n')
+    print(f'Sua palavra tem {numero_letras} letras! \U0001f4a1\n')
     siga = input('Pressione Enter para iniciar o jogo ...')
 
     while contador_erros < tentativas and lugar_para_encontrar != list(palavra_escolhida):
 
-        if tentativas == 6:
-            letra_escolhida = painel()
-        else:
-            letra_escolhida = painel_errado()
+        mostrar_painel()
+        letra_escolhida = input('\n\U0001f449 Escolha uma letra: ').lower()
 
-        if letra_escolhida in palavra_escolhida:
+        if letra_escolhida in lugar_para_encontrar or letra_escolhida in lista_errada:
+            print('Você já escolheu esta letra antes!')
+            sleep(2)
+            continue
+
+        elif letra_escolhida in palavra_escolhida:
             print('Woow! Essa letra existe na sua palavra!')
             sleep(1)
             lista_correta.append(letra_escolhida)
@@ -50,22 +49,22 @@ while True:
             for indice in indices:
                 lugar_para_encontrar[indice] = letra_escolhida
         else:
-            print('Você errou, esta letra não tem na palavra')
+            print('Você errou, essa letra não existe na palavra! \u274C')
             sleep(1)
             tentativas = tentativas - 1
             lista_errada.append(letra_escolhida)
 
-    os.system('cls' if os.name == 'nt' else 'clear')
+    limpar_tela()
 
     if contador_erros >= tentativas:
         print(f'Você perdeu!\n\nA palavra correta era "{palavra_escolhida.upper()}".\n')
     else:
-        print(f'Parabéns! Você acertou a palavra!\n\nA palavra correta era {palavra_escolhida.upper()}!\n')
+        print(f'Parabéns! Você acertou a palavra! \U0001f389\n\nA palavra correta era {palavra_escolhida.upper()}!\n')
 
     jogar_novamente = input('Deseja jogar novamente? (S/N) ')
-    os.system('cls' if os.name == 'nt' else 'clear')
+    limpar_tela()
 
     if jogar_novamente.lower() != 's':
-        os.system('cls' if os.name == 'nt' else 'clear')
-        print('Obrigado por jogar!')
+        limpar_tela()
+        print('\nObrigado por jogar!\n')
         break
